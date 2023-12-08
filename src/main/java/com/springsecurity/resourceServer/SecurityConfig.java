c2155148919435b49e465d496bb4d68498bac426
Noorjahan <noorjahann1996@gmail.com>
2023-12-08 16:22:14 +0530

    OAuth2 integration

--- src/main/java/com/springsecurity/resourceServer/SecurityConfig.java
+++ src/main/java/com/springsecurity/resourceServer/SecurityConfig.java
@@ -3,13 +3,7 @@ package com.springsecurity.resourceServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.nio.file.PathMatcher;
 
 @Configuration
 public class SecurityConfig {
@@ -19,7 +13,7 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests()
-                        .requestMatchers("/test","/","").permitAll()
+                        .requestMatchers("/aboutApp","/","").permitAll()
                         .anyRequest().authenticated()
                         .and()
                                 .oauth2Login();
